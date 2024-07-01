39b657b5a01d5b7f63ae279a90e00d685f1911ac
saiful <eamon831@gmail.com>
2024-07-01 16:42:22 +0600

    splash page added

---
+++ lib/app/pages/splash/views/splash_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '/app/core/base/base_view.dart';
+import '/app/pages/splash/controllers/splash_controller.dart';
+
+//ignore: must_be_immutable
+class SplashView extends BaseView<SplashController> {
+  SplashView({super.key});
+    
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return null;
+  }
+  
+  @override
+  Widget body(BuildContext context) {
+    return Container();
+  }
+}
+  
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
-
+import '/app/pages/splash/bindings/splash_binding.dart';
+import '/app/pages/splash/views/splash_view.dart';
 part 'app_routes.dart';
 
 class AppPages {
@@ -7,5 +8,11 @@ class AppPages {
 
   static const initial = Routes.splash;
 
-  static final routes = [];
+  static final routes = [
+    GetPage(
+      name: Routes.splash,
+      page: SplashView.new,
+      binding: SplashBinding(),
+    ),
+  ];
 }
