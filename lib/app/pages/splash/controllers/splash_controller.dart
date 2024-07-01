e05f11ba9e7405f43c2fe0ec1d22dfa4f1302c93
saiful <eamon831@gmail.com>
2024-07-01 17:14:57 +0600

    one login condition added

--- lib/app/pages/splash/controllers/splash_controller.dart
+++ lib/app/pages/splash/controllers/splash_controller.dart
@@ -1,4 +1,9 @@
+import 'package:get/get.dart';
+import 'package:nb_utils/nb_utils.dart';
+
 import '/app/core/base/base_controller.dart';
+import '/app/core/core_model/logged_user.dart';
+import '/app/core/core_model/setup.dart';
 
 class SplashController extends BaseController {
   @override
@@ -11,5 +16,28 @@ class SplashController extends BaseController {
 
     final bool isLicenseValid = await prefs.getIsLicenseValid();
     final bool isLogin = await prefs.getIsLogin();
+
+    if (isLicenseValid && isLogin) {
+      final setupData = await dbHelper.getAll(tbl: dbTables.tableSetup);
+      SetUp.fromJson(setupData[0]);
+
+      final loggedUserData = await dbHelper.getAllWhr(
+        tbl: dbTables.tableUsers,
+        where: 'user_name==? AND password==?',
+        whereArgs: [
+          await prefs.getLoggedUserName(),
+          await prefs.getLoggedUserPassword(),
+        ],
+      );
+
+      if (loggedUserData.isNotEmpty) {
+        LoggedUser.fromJson(loggedUserData[0]);
+        Get.offAllNamed('');
+      } else {
+        toast('Invalid Credentials');
+      }
+    }
+
+
   }
 }
