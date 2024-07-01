acdde9bcdd4141aa150c41801dcc2dfe1c8f7d57
saiful <eamon831@gmail.com>
2024-07-01 18:26:53 +0600

    app dimension added

---
+++ lib/app/core/values/app_dimension.dart
@@ -0,0 +1,28 @@
+import '/app/core/utils/responsive.dart';
+
+class AppDimension {
+  double containerBorderRadius = 4;
+
+  double appBarTFSize = 24;
+  double headerTFSize = 20;
+  double subHeaderTFSize = 18;
+  double paragraphTFSize = 16;
+  double labelTFSize = 12;
+  double valueTFSize = 12;
+  double regularTFSize = 14;
+  double mediumTFSize = 12;
+  double smallTFSize = 10;
+  double extraSmallTFSize = 8;
+  double largeButtonTFSize = 20;
+  double mediumButtonTFSize = 18;
+  double smallButtonTFSize = 14;
+  double dotMenuTFSize = 10.sp;
+
+  double textFieldHeight = 5.ph;
+  double mediumTextFieldHeight = 4.ph;
+  double smallTextFieldHeight = 3.ph;
+
+  double dropDownFieldHeight = 05.ph;
+  double buttonHeight = 5.ph;
+  double closeIconSize = 24;
+}
---
+++ lib/app/core/widget/row_button.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import '/app/core/utils/responsive.dart';
+
+import 'common_text.dart';
+import 'draw_icon.dart';
+
+class RowButton extends StatelessWidget {
+  final String buttonName;
+  final Color? buttonTextColor;
+  final Color? buttonBGColor;
+  final IconData? leftIcon;
+  final IconData? rightIcon;
+  final VoidCallback onTap;
+  final bool? isOutline;
+
+  const RowButton({
+    required this.buttonName,
+    required this.onTap,
+    super.key,
+    this.buttonTextColor,
+    this.buttonBGColor,
+    this.leftIcon,
+    this.rightIcon,
+    this.isOutline,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          height: 6.ph,
+          padding: const EdgeInsets.only(top: 8, bottom: 8),
+          decoration: BoxDecoration(
+            color: buttonBGColor ??
+                ((isOutline ?? false)
+                    ? colors.backgroundColor
+                    : colors.primaryBaseColor),
+            borderRadius: BorderRadius.circular(containerBorderRadius),
+            border: (isOutline ?? false)
+                ? Border.all(
+                    color: colors.primaryBaseColor,
+                  )
+                : null,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (leftIcon != null)
+                DrawIcon(
+                  icon: leftIcon!,
+                  color: (isOutline ?? false)
+                      ? colors.primaryBaseColor
+                      : colors.backgroundColor,
+                ),
+              Container(
+                margin: const EdgeInsets.only(left: 5, right: 5),
+                child: CommonText(
+                  text: buttonName,
+                  fontWeight: FontWeight.w500,
+                  fontSize: mediumButtonTFSize,
+                  textColor: (isOutline ?? false)
+                      ? colors.primaryBaseColor
+                      : colors.backgroundColor,
+                ),
+              ),
+              if (rightIcon != null)
+                DrawIcon(
+                  icon: rightIcon!,
+                  color: (isOutline ?? false)
+                      ? colors.primaryBaseColor
+                      : colors.backgroundColor,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
