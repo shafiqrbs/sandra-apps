5749b173bc0bbb8c00cece84ef75101579c29f9d
saiful <eamon831@gmail.com>
2024-07-02 12:45:54 +0600

    column icon text added

---
+++ lib/app/core/widget/column_icon_text.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '/app/core/base/base_widget.dart';
+import '/app/core/singleton_classes/color_schema.dart';
+
+class ColumnIconText extends BaseWidget {
+  final bool isSelected;
+  final String text;
+  final Function()? onTap;
+  final IconData? icon;
+
+  ColumnIconText({
+    required this.isSelected,
+    required this.text,
+    required this.onTap,
+    required this.icon,
+    super.key,
+  });
+  final colorSchema = ColorSchema();
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            dimensions.containerBorderRadius,
+          ),
+          border: Border.all(
+            color:
+                isSelected ? colorSchema.borderColor : colorSchema.borderColor,
+          ),
+          color: isSelected ? colorSchema.selectedColor : Colors.transparent,
+        ),
+        child: Column(
+          children: [
+            if (icon != null)
+              Icon(
+                icon,
+                color: isSelected ? Colors.black : colorSchema.iconColor,
+                size: 24,
+              ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: dimensions.mediumTFSize,
+                color: isSelected ? Colors.black : colorSchema.primaryTextColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/global_widget/customer_card_view.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:get/get.dart';
+import 'package:getx_template/app/core/base/base_widget.dart';
+import 'package:getx_template/app/model/customer.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+class CustomerCardView extends BaseWidget {
+  final Customer data;
+  final int index;
+  final Function() onTap;
+  final Function() onReceive;
+  final bool showReceiveButton;
+  CustomerCardView({
+    required this.data,
+    required this.index,
+    required this.onTap,
+    required this.onReceive,
+    required this.showReceiveButton,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 8),
+            padding: EdgeInsets.zero,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  dimensions.containerBorderRadius,
+                ),
+                color: index.isEven
+                    ? colors.evenListColor
+                    : colors.evenListColor.withOpacity(.4),
+                boxShadow: [
+                  BoxShadow(
+                    color: colors.backgroundColor,
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  padding: const EdgeInsets.only(top: 4),
+                                  child: Text(
+                                    data.name ?? '',
+                                    style: TextStyle(
+                                      fontSize: dimensions.paragraphTFSize,
+                                      color: colors.primaryTextColor,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          8.height,
+                          Row(
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: Text(
+                                  data.mobile ?? '',
+                                  style: TextStyle(
+                                    color: colors.secondaryTextColor,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: CommonIconText(
+                                  text: data.balance?.toString() ?? '0',
+                                  textColor: colors.dangerBaseColor,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: dimensions.paragraphTFSize,
+                                  icon: TablerIcons.cash,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: Container(),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 16,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              _buildIconButton(
+                onTap: () =>
+                    UtilityFunctions().makeCall(data.mobile ?? '', context),
+                icon: TablerIcons.phone_outgoing,
+                color: colors.colorTwo,
+              ),
+              _buildIconButton(
+                onTap: () => UtilityFunctions()
+                    .messageCustomer(data.mobile ?? '', context),
+                icon: TablerIcons.message_circle,
+                color: colors.colorFour,
+              ),
+              _buildIconButton(
+                onTap: () {},
+                icon: TablerIcons.eye,
+                color: colors.primaryBaseColor,
+              ),
+              if (showReceiveButton)
+                _buildIconButton(
+                  onTap: () {},
+                  icon: TablerIcons.coin_bitcoin,
+                  color: colors.primaryBaseColor,
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildIconButton({
+    required VoidCallback onTap,
+    required IconData icon,
+    required Color color,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.only(left: 4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(
+            color: colors.primaryLiteColor.withOpacity(.5),
+          ),
+        ),
+        child: Icon(
+          icon,
+          size: 20,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
