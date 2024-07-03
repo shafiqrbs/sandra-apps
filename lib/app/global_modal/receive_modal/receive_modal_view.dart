69ee012df7a48e4e09f6f2fb2a90bf526b200814
saiful <eamon831@gmail.com>
2024-07-03 20:25:58 +0600

    receive modal controller refactored

--- lib/app/global_modal/receive_modal/receive_modal_view.dart
+++ lib/app/global_modal/receive_modal/receive_modal_view.dart
@@ -21,192 +21,203 @@ class ReceiveModalView extends BaseView<ReceiveModalController> {
 
   @override
   Widget build(BuildContext context) {
-    final mvc = Get.put(
-      ReceiveModalController(
+    return GetX<ReceiveModalController>(
+      init: ReceiveModalController(
         customer: customer,
       ),
-    );
-    return SingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: colors.backgroundColor,
-        ),
-        child: Column(
-          children: [
-            FBString(
-              isRequired: false,
-              textController: mvc.customerManager.searchTextController.value,
-              onChange: (value) async {
-                if (value?.isEmpty ?? true) {
-                  mvc.customerManager.searchedItems.value = [];
-                  mvc.customerManager.selectedItem.value = null;
-                  return;
-                }
-                await mvc.customerManager.searchItemsByName(value!);
-
-                print('search: $value');
-                //print('search: ${mvc.customerManager.allItems.value!.length}');
-              },
-              hint: 'search_customer'.tr,
-              suffixIcon: TablerIcons.search,
+      builder: (controller) {
+        return SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: colors.backgroundColor,
             ),
-            1.percentHeight,
-            Stack(
+            child: Column(
               children: [
-                Column(
+                FBString(
+                  isRequired: false,
+                  textController:
+                      controller.customerManager.searchTextController.value,
+                  onChange: (value) async {
+                    if (value?.isEmpty ?? true) {
+                      controller.customerManager.searchedItems.value = [];
+                      controller.customerManager.selectedItem.value = null;
+                      return;
+                    }
+                    await controller.customerManager.searchItemsByName(value!);
+
+                    print('search: $value');
+                    //print('search: ${controller.customerManager.allItems.value!.length}');
+                  },
+                  hint: 'search_customer'.tr,
+                  suffixIcon: TablerIcons.search,
+                ),
+                1.percentHeight,
+                Stack(
                   children: [
-                    Obx(
-                      () => mvc.customerManager.selectedItem.value != null
-                          ? Column(
-                              children: [
-                                CustomerCardView(
-                                  data: mvc.customerManager.selectedItem.value!,
-                                  index: 0,
-                                  onTap: () {},
-                                  onReceive: () {},
-                                  showReceiveButton: false,
-                                ),
-                                1.percentHeight,
-                              ],
-                            )
-                          : Container(),
-                    ),
-                    Obx(
-                      () => Column(
-                        children: [
-                          1.percentHeight,
-                          if (mvc.transactionMethodsManager.allItems.value !=
-                              null)
-                            SingleChildScrollView(
-                              padding: const EdgeInsets.all(8),
-                              scrollDirection: Axis.horizontal,
-                              child: Wrap(
-                                spacing: 8,
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                                runSpacing: 8,
-                                children: mvc
-                                    .transactionMethodsManager.allItems.value!
-                                    .map(
-                                  (e) {
-                                    final selected = mvc
+                    Column(
+                      children: [
+                        Obx(
+                          () => controller.customerManager.selectedItem.value !=
+                                  null
+                              ? Column(
+                                  children: [
+                                    CustomerCardView(
+                                      data: controller
+                                          .customerManager.selectedItem.value!,
+                                      index: 0,
+                                      onTap: () {},
+                                      onReceive: () {},
+                                      showReceiveButton: false,
+                                    ),
+                                    1.percentHeight,
+                                  ],
+                                )
+                              : Container(),
+                        ),
+                        Obx(
+                          () => Column(
+                            children: [
+                              1.percentHeight,
+                              if (controller.transactionMethodsManager.allItems
+                                      .value !=
+                                  null)
+                                SingleChildScrollView(
+                                  padding: const EdgeInsets.all(8),
+                                  scrollDirection: Axis.horizontal,
+                                  child: Wrap(
+                                    spacing: 8,
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    runSpacing: 8,
+                                    children: controller
                                         .transactionMethodsManager
-                                        .selectedItem
-                                        .value;
-                                    return TransactionMethodItemView(
-                                      method: e,
-                                      isSelected: selected == e,
-                                      onTap: () {
-                                        mvc.transactionMethodsManager
-                                            .selectedItem.value = e;
+                                        .allItems
+                                        .value!
+                                        .map(
+                                      (e) {
+                                        final selected = controller
+                                            .transactionMethodsManager
+                                            .selectedItem
+                                            .value;
+                                        return TransactionMethodItemView(
+                                          method: e,
+                                          isSelected: selected == e,
+                                          onTap: () {
+                                            controller.transactionMethodsManager
+                                                .selectedItem.value = e;
+                                          },
+                                        );
                                       },
-                                    );
-                                  },
-                                ).toList(),
+                                    ).toList(),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 4),
+                          alignment: Alignment.center,
+                          height: 50,
+                          child: TextFormField(
+                            controller: controller.amountController.value,
+                            inputFormatters: [regexDouble],
+                            keyboardType: TextInputType.number,
+                            textInputAction: TextInputAction.done,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w400,
+                            ),
+                            cursorColor: colors.formCursorColor,
+                            decoration: InputDecoration(
+                              hintText: 'Amount',
+                              filled: true,
+                              fillColor: colors.textFieldColor,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(4),
+                                borderSide: BorderSide(
+                                  color: colors.primaryBaseColor,
+                                  width: 2,
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(4),
+                                borderSide: BorderSide(
+                                  color: colors.borderColor,
+                                  width: 2,
+                                ),
                               ),
                             ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 4),
-                      alignment: Alignment.center,
-                      height: 50,
-                      child: TextFormField(
-                        controller: mvc.amountController.value,
-                        inputFormatters: [regexDouble],
-                        keyboardType: TextInputType.number,
-                        textInputAction: TextInputAction.done,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w400,
-                        ),
-                        cursorColor: colors.formCursorColor,
-                        decoration: InputDecoration(
-                          hintText: 'Amount',
-                          filled: true,
-                          fillColor: colors.textFieldColor,
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                            borderSide: BorderSide(
-                              color: colors.primaryBaseColor,
-                              width: 2,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                            borderSide: BorderSide(
-                              color: colors.borderColor,
-                              width: 2,
-                            ),
+                            onChanged: (value) {},
                           ),
                         ),
-                        onChanged: (value) {},
-                      ),
-                    ),
-                    1.percentHeight,
-                    FBString(
-                      textController: mvc.addRemarkController.value,
-                      isRequired: false,
-                      lines: 2,
-                      hint: 'add_remark'.tr,
-                    ),
-                    1.percentHeight,
-                    Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        color: colors.moduleFooterColor,
-                      ),
-                      //height: 100,
-                      width: Get.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RowButton(
-                            buttonName: 'reset'.tr,
-                            leftIcon: TablerIcons.restore,
-                            onTap: mvc.resetField,
-                            isOutline: true,
+                        1.percentHeight,
+                        FBString(
+                          textController: controller.addRemarkController.value,
+                          isRequired: false,
+                          lines: 2,
+                          hint: 'add_remark'.tr,
+                        ),
+                        1.percentHeight,
+                        Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: colors.moduleFooterColor,
                           ),
-                          16.width,
-                          RowButton(
-                            buttonName: 'save'.tr,
-                            leftIcon: TablerIcons.device_floppy,
-                            onTap: mvc.processReceive,
-                            isOutline: false,
+                          //height: 100,
+                          width: Get.width,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RowButton(
+                                buttonName: 'reset'.tr,
+                                leftIcon: TablerIcons.restore,
+                                onTap: controller.resetField,
+                                isOutline: true,
+                              ),
+                              16.width,
+                              RowButton(
+                                buttonName: 'save'.tr,
+                                leftIcon: TablerIcons.device_floppy,
+                                onTap: controller.processReceive,
+                                isOutline: false,
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
+                      ],
+                    ),
+                    Obx(
+                      () => ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: controller
+                                .customerManager.searchedItems.value?.length ??
+                            0,
+                        itemBuilder: (context, index) {
+                          return CustomerCardView(
+                            data: controller
+                                .customerManager.searchedItems.value![index],
+                            index: index,
+                            onTap: () {
+                              controller.updateCustomer(
+                                controller.customerManager.searchedItems
+                                    .value![index],
+                              );
+                              //close keyboard
+                              FocusScope.of(context).unfocus();
+                            },
+                            onReceive: () {},
+                            showReceiveButton: false,
+                          );
+                        },
                       ),
                     ),
                   ],
                 ),
-                Obx(
-                  () => ListView.builder(
-                    shrinkWrap: true,
-                    itemCount:
-                        mvc.customerManager.searchedItems.value?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      return CustomerCardView(
-                        data: mvc.customerManager.searchedItems.value![index],
-                        index: index,
-                        onTap: () {
-                          mvc.updateCustomer(
-                            mvc.customerManager.searchedItems.value![index],
-                          );
-                          //close keyboard
-                          FocusScope.of(context).unfocus();
-                        },
-                        onReceive: () {},
-                        showReceiveButton: false,
-                      );
-                    },
-                  ),
-                ),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 
