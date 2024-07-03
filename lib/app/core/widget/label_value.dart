8c39c766bd0e156c70d9a1a8d9005f403b0223b9
saiful <eamon831@gmail.com>
2024-07-03 13:30:15 +0600

    label value added

---
+++ lib/app/core/widget/label_value.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '/app/core/extensions/text_style_extensions.dart';
+import '/app/core/utils/responsive.dart';
+
+class LabelValue extends StatelessWidget {
+  final String? label;
+  final String? value;
+  final String? dividerText;
+  final double? labelFontSize;
+  final double? valueFontSize;
+  final int? labelFlex;
+  final int? valueFlex;
+  final TextAlign? valueTextAlign;
+  final TextAlign? labelTextAlign;
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
+
+  final int? labelFontWeight;
+  final int? valueFontWeight;
+  final Color? labelColor;
+  final Color? valueColor;
+  final Color? borderColor;
+  final double? borderWidth;
+
+  final String? fontFamily;
+
+  const LabelValue({
+    required this.label,
+    required this.value,
+    super.key,
+    this.dividerText,
+    this.labelFontSize = 14,
+    this.valueFontSize,
+    this.labelFlex,
+    this.valueFlex,
+    this.labelFontWeight,
+    this.valueFontWeight,
+    this.labelColor,
+    this.valueColor,
+    this.labelTextAlign,
+    this.valueTextAlign,
+    this.margin,
+    this.padding,
+    this.borderColor,
+    this.borderWidth,
+    this.fontFamily,
+  });
+
+  LabelValue copyWith({
+    String? label,
+    String? value,
+    String? dividerText,
+    double? labelFontSize,
+    double? valueFontSize,
+    int? labelFlex,
+    int? valueFlex,
+    TextAlign? valueTextAlign,
+    TextAlign? labelTextAlign,
+    EdgeInsets? margin,
+    EdgeInsets? padding,
+    int? labelFontWeight,
+    int? valueFontWeight,
+    Color? labelColor,
+    Color? valueColor,
+    Color? borderColor,
+    double? borderWidth,
+    String? fontFamily,
+  }) {
+    return LabelValue(
+      key: key,
+      label: label ?? this.label,
+      value: value ?? this.value,
+      dividerText: dividerText ?? this.dividerText,
+      labelFontSize: labelFontSize ?? this.labelFontSize,
+      valueFontSize: valueFontSize ?? this.valueFontSize,
+      labelFlex: labelFlex ?? this.labelFlex,
+      valueFlex: valueFlex ?? this.valueFlex,
+      labelFontWeight: labelFontWeight ?? this.labelFontWeight,
+      valueFontWeight: valueFontWeight ?? this.valueFontWeight,
+      labelColor: labelColor ?? this.labelColor,
+      valueColor: valueColor ?? this.valueColor,
+      labelTextAlign: labelTextAlign ?? this.labelTextAlign,
+      valueTextAlign: valueTextAlign ?? this.valueTextAlign,
+      margin: margin ?? this.margin,
+      padding: padding ?? this.padding,
+      borderColor: borderColor ?? this.borderColor,
+      borderWidth: borderWidth ?? this.borderWidth,
+      fontFamily: fontFamily ?? this.fontFamily,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: margin ?? EdgeInsets.zero,
+      padding: padding ?? const EdgeInsets.only(right: 5, top: 5, bottom: 5),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor ?? Colors.transparent,
+          width: borderWidth ?? 1,
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: labelFlex ?? 1,
+            child: Text(
+              label ?? 'Empty Label',
+              textAlign: labelTextAlign,
+              style: TextStyle(
+                fontSize: labelFontSize?.fSize ?? 16.fSize,
+                fontWeight: defaultFontWeight[labelFontWeight ?? 500],
+                color: labelColor ?? Colors.black,
+                fontFamily: fontFamily ?? GoogleFonts.inter().fontFamily,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 5),
+            child: Text(
+              dividerText ?? ':',
+              style: TextStyle(
+                fontSize: valueFontSize ?? 16.fSize,
+                color: valueColor ?? Colors.black,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: valueFlex ?? 2,
+            child: Container(
+              margin: const EdgeInsets.only(left: 5),
+              child: Text(
+                value ?? 'Empty Value',
+                textAlign: valueTextAlign,
+                style: TextStyle(
+                  fontSize: valueFontSize?.fSize ?? 15.fSize,
+                  fontWeight: defaultFontWeight[valueFontWeight ?? 500],
+                  color: valueColor ?? Colors.black.withOpacity(0.6),
+                  fontFamily: fontFamily ?? GoogleFonts.inter().fontFamily,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/pages/stock_list/component/stock_card_view.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:get/get.dart';
+import 'package:getx_template/app/core/base/base_widget.dart';
+import 'package:getx_template/app/core/widget/common_icon_text.dart';
+import 'package:getx_template/app/model/stock.dart';
+
+class StockCardView extends BaseWidget {
+  final Stock element;
+  final int index;
+  final Rx<bool> isSelectedItem;
+  final Rx<bool> isBookmarked;
+
+  StockCardView({
+    required this.element,
+    required this.index,
+    required this.isSelectedItem,
+    required this.isBookmarked,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        //show modal
+        toast('StockCardView');
+
+        showDialog(
+          context: context,
+          builder: (context) {
+            return DialogPattern(
+              title: 'title',
+              subTitle: 'subTitle',
+              child: StockDetailsModaL(
+                element: element,
+              ),
+            );
+          },
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            // width: Get.width,
+            // height: Get.height * .072,
+            padding: const EdgeInsets.only(
+              left: 4,
+              right: 4,
+              bottom: 2,
+              top: 2,
+            ),
+            margin: const EdgeInsets.only(
+              bottom: 4,
+              left: 6,
+              right: 6,
+            ),
+            decoration: BoxDecoration(
+              color: index.isEven ? colors.evenListColor : colors.oddListColor,
+              borderRadius: BorderRadius.circular(
+                containerBorderRadius,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color(0x0ff3e9e6),
+                  blurRadius: 2,
+                  offset: Offset(
+                    2,
+                    2,
+                  ), // shadow direction: bottom right
+                ),
+              ],
+            ),
+
+            child: Container(
+              width: Get.width,
+              padding: const EdgeInsets.only(
+                top: 4,
+                bottom: 4,
+                left: 12,
+              ),
+              decoration: BoxDecoration(
+                color:
+                    index.isEven ? colors.evenListColor : colors.oddListColor,
+                borderRadius: BorderRadius.circular(
+                  containerBorderRadius,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${element.name ?? ""} ",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      color: colors.primaryTextColor,
+                    ),
+                  ),
+                  3.height,
+                  Row(
+                    crossAxisAlignment: startCAA,
+                    children: [
+                      Expanded(
+                        child: CommonIconText(
+                          icon: TablerIcons.tag,
+                          iconSize: 16,
+                          iconColor: colors.primaryTextColor,
+                          text: element.brandName ?? 'No Brand',
+                        ),
+                      ),
+                      Expanded(
+                        child: CommonIconText(
+                          icon: TablerIcons.category,
+                          iconSize: 16,
+                          iconColor: colors.primaryTextColor,
+                          text: element.categoryName ?? 'No Category',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: Get.width,
+                    height: 1,
+                    margin: const EdgeInsets.only(
+                      left: 4,
+                      right: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: colors.borderColor,
+                    ),
+                  ),
+                  9.height,
+                  Row(
+                    crossAxisAlignment: startCAA,
+                    children: [
+                      Expanded(
+                        child: labelValue.copyWith(
+                          label: 'stock'.tr,
+                          value: element.quantity.toString(),
+                        ),
+                      ),
+                      Expanded(
+                        child: labelValue.copyWith(
+                          label: 'purchase'.tr,
+                          value: element.purchasePrice.toString(),
+                        ),
+                      ),
+                      Expanded(
+                        child: labelValue.copyWith(
+                          label: 'mrp'.tr,
+                          value: element.salesPrice.toString(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: Get.width * .035,
+            top: Get.height * .02,
+            child: InkWell(
+              onTap: () {
+                //show modal
+              },
+              child: const Icon(
+                TablerIcons.chevron_right,
+                size: 24,
+                color: Color(0xFF000000),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
