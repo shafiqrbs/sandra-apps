4db388076e8f955f5eb22f876bf733db3e68c7e9
saiful <eamon831@gmail.com>
2024-07-03 17:13:41 +0600

    customer list view drawn

--- lib/app/pages/customer_list/controllers/customer_list_controller.dart
+++ lib/app/pages/customer_list/controllers/customer_list_controller.dart
@@ -1,11 +1,22 @@
 import 'package:get/get.dart';
-  import '/app/core/base/base_controller.dart';
-  
+import 'package:getx_template/app/model/customer.dart';
+import '/app/core/base/base_controller.dart';
+
 class CustomerListController extends BaseController {
- @override
+  final customerManager = CustomerManager();
+  final isSearchSelected = false.obs;
+
+  @override
   Future<void> onInit() async {
     super.onInit();
+    await customerManager.paginate();
   }
 
+  void showAddCustomerModal() {}
+
+  Future<void> onClearSearchText() async {
+    customerManager.searchTextController.value.clear();
+    isSearchSelected.value = false;
+    await customerManager.paginate();
+  }
 }
-  
--- lib/app/pages/customer_list/views/customer_list_view.dart
+++ lib/app/pages/customer_list/views/customer_list_view.dart
@@ -1,20 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_template/app/core/widget/add_button.dart';
+import 'package:getx_template/app/core/widget/app_bar_button_group.dart';
+import 'package:getx_template/app/core/widget/app_bar_search_view.dart';
+import 'package:getx_template/app/core/widget/common_icon_text.dart';
+import 'package:getx_template/app/core/widget/quick_navigation_button.dart';
+import 'package:getx_template/app/core/widget/search_button.dart';
 import '/app/core/base/base_view.dart';
 import '/app/pages/customer_list/controllers/customer_list_controller.dart';
 
 //ignore: must_be_immutable
 class CustomerListView extends BaseView<CustomerListController> {
   CustomerListView({super.key});
-    
+
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
-    return null;
+    return AppBar(
+      centerTitle: false,
+      backgroundColor: colors.primaryBaseColor,
+      title: Obx(
+        () {
+          return AppBarSearchView(
+            pageTitle: 'customer'.tr,
+            controller: controller.customerManager.searchTextController.value,
+            onSearch: controller.customerManager.searchItemsByNameOnAllItem,
+            onMicTap: controller.isSearchSelected.toggle,
+            onFilterTap: () {},
+            onClearTap: controller.onClearSearchText,
+            showSearchView: controller.isSearchSelected.value,
+          );
+        },
+      ),
+      automaticallyImplyLeading: false,
+      actions: [
+        Obx(
+          () {
+            if (controller.isSearchSelected.value) {
+              return Container();
+            }
+            return AppBarButtonGroup(
+              children: [
+                AddButton(
+                  onTap: controller.showAddCustomerModal,
+                ),
+                SearchButton(
+                  onTap: controller.isSearchSelected.toggle,
+                ),
+                QuickNavigationButton(),
+              ],
+            );
+          },
+        ),
+      ],
+    );
   }
-  
+
   @override
   Widget body(BuildContext context) {
-    return Column();
+    return Obx(
+      () {
+        return ListView.builder(
+          itemCount: controller.customerManager.allItems.value?.length ?? 0,
+          controller: controller.customerManager.scrollController,
+          padding: const EdgeInsets.only(bottom: 60),
+          itemBuilder: (context, index) {
+            final element = controller.customerManager.allItems.value![index];
+
+            return InkWell(
+              onTap: () async {},
+              child: Container(
+                margin: const EdgeInsets.all(4),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: index.isEven
+                      ? colors.evenListColor
+                      : colors.oddListColor,
+                  borderRadius: BorderRadius.circular(containerBorderRadius),
+                ),
+                child: Column(
+                  children: [
+                    Text(element.name ?? ''),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 }
-  
