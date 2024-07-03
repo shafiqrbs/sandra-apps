dd6720f741e941c77f08d47c9945e91e55d54941
saiful <eamon831@gmail.com>
2024-07-03 19:09:49 +0600

    shimmer list view added

---
+++ lib/app/global_widget/shimmer_list_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerListView extends StatelessWidget {
+  final int itemCount;
+  final String msg;
+  const ShimmerListView({
+    required this.itemCount,
+    required this.msg,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: itemCount,
+        itemBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            width: 200,
+            height: 100,
+            child: Shimmer.fromColors(
+              baseColor: Colors.red,
+              highlightColor: Colors.yellow,
+              child: Text(
+                msg,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
