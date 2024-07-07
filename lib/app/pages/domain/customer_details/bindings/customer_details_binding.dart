0e5e200ddbb20728e9a61880bc7187d2d029d16e
saiful <eamon831@gmail.com>
2024-07-07 16:21:41 +0600

    customer details replaced

--- lib/app/pages/domain/customer_details/bindings/customer_details_binding.dart
+++ lib/app/pages/domain/customer_details/bindings/customer_details_binding.dart
@@ -1,11 +1,11 @@
 import 'package:get/get.dart';
-import '/app/pages/customer_details/controllers/customer_details_controller.dart';
+import '/app/pages/domain/customer_details/controllers/customer_details_controller.dart';
 
 class CustomerDetailsBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<CustomerDetailsController>(
-      () => CustomerDetailsController(),
+      CustomerDetailsController.new,
       fenix: true,
     );
   }
--- lib/app/pages/domain/customer_details/views/customer_details_view.dart
+++ lib/app/pages/domain/customer_details/views/customer_details_view.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import '/app/core/base/base_view.dart';
-import '/app/pages/customer_details/controllers/customer_details_controller.dart';
+import '/app/pages/domain/customer_details/controllers/customer_details_controller.dart';
 
 //ignore: must_be_immutable
 class CustomerDetailsView extends BaseView<CustomerDetailsController> {
   CustomerDetailsView({super.key});
-    
+
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return null;
   }
-  
+
   @override
   Widget body(BuildContext context) {
     return Container();
   }
 }
-  
