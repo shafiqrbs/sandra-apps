31b85c0a06fc1fdbeb254da244b787b65e1c9fed
saiful <eamon831@gmail.com>
2024-07-07 20:14:38 +0600

    build.sh added

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Clean the Flutter project
+flutter clean
+
+# Get Flutter dependencies
+flutter pub get
+echo "Building Android..."
+
+# Build the APK in release mode
+flutter build apk --release
+
+# Get the current date and time in the desired format
+current_date=$(date "+%Y-%m-%d_%I:%M:%S_%p")
+
+# Replace colons with hyphens to ensure it's a valid filename
+sanitized_date=$(echo $current_date | tr ':' '-')
+
+# Copy the built APK to a new file with a timestamped filename
+cp build/app/outputs/flutter-apk/app-release.apk "sandra_${sanitized_date}.apk"
+
+# Output a message indicating that the build is complete
+echo "APK created successfully"
