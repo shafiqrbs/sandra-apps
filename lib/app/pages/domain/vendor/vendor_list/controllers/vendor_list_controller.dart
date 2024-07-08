e54a733306f1b51659de1db01567029a9004a6d9
saiful <eamon831@gmail.com>
2024-07-08 19:40:38 +0600

    vendor list view page drawn

--- lib/app/pages/domain/vendor/vendor_list/controllers/vendor_list_controller.dart
+++ lib/app/pages/domain/vendor/vendor_list/controllers/vendor_list_controller.dart
@@ -1,11 +1,31 @@
 import 'package:get/get.dart';
-  import '/app/core/base/base_controller.dart';
-  
+import 'package:getx_template/app/core/widget/dialog_pattern.dart';
+import 'package:getx_template/app/global_modal/add_vendor_modal/add_vendor_modal_view.dart';
+import 'package:getx_template/app/model/vendor.dart';
+import '/app/core/base/base_controller.dart';
+
 class VendorListController extends BaseController {
- @override
+  final vendorManager = VendorManager();
+  final isSearchSelected = false.obs;
+  @override
   Future<void> onInit() async {
     super.onInit();
+    await vendorManager.paginate();
   }
 
+  void showAddVendorModal() {
+    final result = Get.dialog(
+      DialogPattern(
+        title: 'title',
+        subTitle: '',
+        child: AddVendorModalView(),
+      ),
+    );
+  }
+
+  Future<void> onClearSearchText() async {
+    vendorManager.searchTextController.value.clear();
+    isSearchSelected.value = false;
+    await vendorManager.paginate();
+  }
 }
-  
--- lib/app/pages/domain/vendor/vendor_list/views/vendor_list_view.dart
+++ lib/app/pages/domain/vendor/vendor_list/views/vendor_list_view.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_template/app/core/widget/add_button.dart';
+import 'package:getx_template/app/core/widget/app_bar_button_group.dart';
+import 'package:getx_template/app/core/widget/app_bar_search_view.dart';
+import 'package:getx_template/app/core/widget/quick_navigation_button.dart';
+import 'package:getx_template/app/core/widget/search_button.dart';
+import 'package:getx_template/app/global_widget/vendor_card_view.dart';
+import 'package:getx_template/app/routes/app_pages.dart';
 import '/app/core/base/base_view.dart';
 import '/app/pages/domain/vendor/vendor_list/controllers/vendor_list_controller.dart';
 
@@ -8,11 +16,73 @@ class VendorListView extends BaseView<VendorListController> {
 
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
-    return null;
+    return AppBar(
+      centerTitle: false,
+      backgroundColor: colors.primaryBaseColor,
+      title: Obx(
+            () {
+          return AppBarSearchView(
+            pageTitle: 'customer'.tr,
+            controller: controller.vendorManager.searchTextController.value,
+            onSearch: controller.vendorManager.searchItemsByNameOnAllItem,
+            onMicTap: controller.isSearchSelected.toggle,
+            onFilterTap: () {},
+            onClearTap: controller.onClearSearchText,
+            showSearchView: controller.isSearchSelected.value,
+          );
+        },
+      ),
+      automaticallyImplyLeading: false,
+      actions: [
+        Obx(
+              () {
+            if (controller.isSearchSelected.value) {
+              return Container();
+            }
+            return AppBarButtonGroup(
+              children: [
+                AddButton(
+                  onTap: controller.showAddVendorModal,
+                ),
+                SearchButton(
+                  onTap: controller.isSearchSelected.toggle,
+                ),
+                QuickNavigationButton(),
+              ],
+            );
+          },
+        ),
+      ],
+    );
   }
 
   @override
   Widget body(BuildContext context) {
-    return Container();
+    return Obx(
+          () {
+        return ListView.builder(
+          itemCount: controller.vendorManager.allItems.value?.length ?? 0,
+          controller: controller.vendorManager.scrollController,
+          padding: const EdgeInsets.only(bottom: 60),
+          itemBuilder: (context, index) {
+            final element = controller.vendorManager.allItems.value![index];
+            return VendorCardView(
+              data: element,
+              index: index,
+              onTap: () {
+                Get.toNamed(
+                  Routes.customerDetails,
+                  arguments: {
+                    'vendor': element,
+                  },
+                );
+              },
+              onReceive: () {},
+              showReceiveButton: true,
+            );
+          },
+        );
+      },
+    );
   }
 }
