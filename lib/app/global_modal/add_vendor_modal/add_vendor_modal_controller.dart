2a8b11e9570631aa2b12f92891740b002683cb67
saiful <eamon831@gmail.com>
2024-07-08 19:20:00 +0600

    add vendor modal added

---
+++ lib/app/global_modal/add_vendor_modal/add_vendor_modal_controller.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '/app/core/base/base_controller.dart';
+import '/app/model/customer.dart';
+
+class AddVendorModalController extends BaseController {
+  final formKey = GlobalKey<FormState>();
+
+  final userNameController = TextEditingController().obs;
+
+  final mobileController = TextEditingController().obs;
+
+  final openingBalanceController = TextEditingController().obs;
+
+  final emailController = TextEditingController().obs;
+
+  final addressController = TextEditingController().obs;
+
+  final isUserNameFieldValid = true.obs;
+
+  Customer? createdCustomer;
+
+  Future<void> addCustomer() async {
+    if (formKey.currentState!.validate()) {
+      await dataFetcher(
+        future: () async {
+          final response = await services.addCustomer(
+            shouldShowLoader: true,
+            name: userNameController.value.text,
+            mobile: mobileController.value.text,
+            address: addressController.value.text,
+            email: emailController.value.text,
+            openingBalance: openingBalanceController.value.text,
+          );
+          if (response != null) {
+            await dbHelper.insertList(
+              deleteBeforeInsert: false,
+              tableName: dbTables.tableCustomers,
+              dataList: [
+                response.toJson(),
+              ],
+            );
+            createdCustomer = response;
+          }
+        },
+      );
+      if (createdCustomer != null) {
+        Get.back(
+          result: createdCustomer,
+        );
+      }
+    }
+  }
+
+  void resetField() {
+    userNameController.value.clear();
+    mobileController.value.clear();
+    openingBalanceController.value.clear();
+    emailController.value.clear();
+    addressController.value.clear();
+    Get.back(result: true);
+  }
+}
