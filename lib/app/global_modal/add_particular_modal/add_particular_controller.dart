2309105f44e9dc7b91aff21ede2f693f172c6764
saiful <eamon831@gmail.com>
2024-07-10 18:56:40 +0600

    add particular modal added

---
+++ lib/app/global_modal/add_particular_modal/add_particular_controller.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_template/app/entity/transaction_methods.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import '/app/core/base/base_controller.dart';
+import '/app/entity/expense_category.dart';
+import '/app/entity/user.dart';
+import '/app/routes/app_pages.dart';
+
+class AddParticularController extends BaseController {
+  final formKey = GlobalKey<FormState>();
+
+  final userManager = UserManager();
+  final expenseCategoryManager = ExpenseCategoryManager();
+  final transactionMethodsManager = TransactionMethodsManager();
+
+  final amountController = TextEditingController().obs;
+  final remarkController = TextEditingController();
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    await expenseCategoryManager.fillAsController();
+    await userManager.fillAsController();
+    await transactionMethodsManager.getAll();
+  }
+
+  void goToListPage() {
+    Get.offNamed(
+      Routes.expenseList,
+    );
+  }
+
+  void onResetTap() {}
+
+  void onSaveTap() {
+    if (formKey.currentState!.validate()) {
+
+      if(transactionMethodsManager.selectedItem.value == null){
+        toast('Please select a transaction method');
+        return;
+      }
+
+      toast('Field is valid');
+      toast('Under development the API');
+     // Get.back();
+    }
+  }
+}
---
+++ lib/app/global_modal/add_particular_modal/add_particular_view.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:get/get.dart';
+import 'package:getx_template/app/core/utils/responsive.dart';
+import 'package:getx_template/app/core/widget/row_button.dart';
+import 'package:getx_template/app/global_widget/transaction_method_item_view.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import '/app/core/advance_select/advance_select_view.dart';
+import '/app/core/base/base_view.dart';
+import '/app/core/widget/app_bar_button_group.dart';
+import '/app/core/widget/fb_string.dart';
+import '/app/core/widget/list_button.dart';
+import '/app/core/widget/quick_navigation_button.dart';
+import 'add_particular_controller.dart';
+
+//ignore: must_be_immutable
+class AddParticularView extends BaseView<AddParticularController> {
+  AddParticularView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetX<AddParticularController>(
+      init: AddParticularController(),
+      builder: (controller) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Form(
+              key: controller.formKey,
+              child: Column(
+                children: [
+                  AdvanceSelect(
+                    isRequired: true,
+                    controller: controller.userManager.asController,
+                    hint: 'user'.tr,
+                    isShowSearch: false,
+                    itemToString: (data) => data?.username ?? '',
+                    label: 'user'.tr,
+                    errorMsg: 'user_required'.tr,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: AdvanceSelect(
+                          isRequired: true,
+                          controller: controller.expenseCategoryManager.asController,
+                          hint: 'select_category'.tr,
+                          isShowSearch: false,
+                          itemToString: (data) => data?.name ?? '',
+                          label: 'category'.tr,
+                          errorMsg: 'category_required'.tr,
+                        ),
+                      ),
+                      10.width,
+                      Expanded(
+                        child: FBString(
+                          textController: controller.amountController.value,
+                          keyboardType: numberInputType,
+                          label: 'amount'.tr,
+                          //example: 'sample_model'.tr,
+                          hint: 'amount'.tr,
+                          isRequired: true,
+                          errorMsg: '',
+                        ),
+                      ),
+                    ],
+                  ),
+                  Obx(
+                    () {
+                      return Column(
+                        children: [
+                          if (controller
+                                  .transactionMethodsManager.allItems.value !=
+                              null)
+                            SingleChildScrollView(
+                              padding: EdgeInsets.zero,
+                              scrollDirection: Axis.horizontal,
+                              child: Wrap(
+                                spacing: 8,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                runSpacing: 8,
+                                children: controller
+                                    .transactionMethodsManager.allItems.value!
+                                    .map(
+                                  (e) {
+                                    final selected = controller
+                                        .transactionMethodsManager
+                                        .selectedItem
+                                        .value;
+                                    return TransactionMethodItemView(
+                                      method: e,
+                                      isSelected: selected == e,
+                                      onTap: () {
+                                        controller.transactionMethodsManager
+                                            .selectedItem.value = e;
+                                      },
+                                    );
+                                  },
+                                ).toList(),
+                              ),
+                            ),
+                        ],
+                      );
+                    },
+                  ),
+                  FBString(
+                    textController: controller.remarkController,
+                    label: 'remark'.tr,
+                    //example: 'sample_model'.tr,
+                    hint: 'remark'.tr,
+                    isRequired: false,
+                    errorMsg: '',
+                    lines: 4,
+                  ),
+                  Row(
+                    children: [
+                      RowButton(
+                        buttonName: 'reset'.tr,
+                        onTap: controller.onResetTap,
+                        leftIcon: TablerIcons.restore,
+                      ),
+                      2.percentWidth,
+                      RowButton(
+                        buttonName: 'save'.tr,
+                        onTap: controller.onSaveTap,
+                        leftIcon: TablerIcons.device_floppy,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    // TODO: implement appBar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget body(BuildContext context) {
+    // TODO: implement body
+    throw UnimplementedError();
+  }
+}
