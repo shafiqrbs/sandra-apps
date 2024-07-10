5ea80781022dbf7f89e976bd7e3c00dd2991db18
saiful <eamon831@gmail.com>
2024-07-10 17:25:31 +0600

    adding create expense view

--- lib/app/pages/accounting/expense/create_expense/controllers/create_expense_controller.dart
+++ lib/app/pages/accounting/expense/create_expense/controllers/create_expense_controller.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_template/app/entity/user.dart';
 import 'package:getx_template/app/routes/app_pages.dart';
 import '/app/core/base/base_controller.dart';
 
 class CreateExpenseController extends BaseController {
+  final userManager = UserManager();
+  final amountController = TextEditingController();
   @override
   Future<void> onInit() async {
     super.onInit();
--- lib/app/pages/accounting/expense/create_expense/views/create_expense_view.dart
+++ lib/app/pages/accounting/expense/create_expense/views/create_expense_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_template/app/core/advance_select/advance_select_view.dart';
 import 'package:getx_template/app/core/widget/app_bar_button_group.dart';
+import 'package:getx_template/app/core/widget/fb_string.dart';
 import 'package:getx_template/app/core/widget/list_button.dart';
 import 'package:getx_template/app/core/widget/quick_navigation_button.dart';
+import 'package:nb_utils/nb_utils.dart';
 import '/app/core/base/base_view.dart';
 import '/app/pages/accounting/expense/create_expense/controllers/create_expense_controller.dart';
 
@@ -39,6 +42,29 @@ class CreateExpenseView extends BaseView<CreateExpenseController> {
 
   @override
   Widget body(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          AdvanceSelect(
+            isRequired: true,
+            controller: controller.userManager.asController,
+            hint: 'select_category'.tr,
+            isShowSearch: false,
+            itemToString: (data) => data?.username ?? '',
+            label: 'category'.tr,
+            errorMsg: 'category_required'.tr,
+          ),
+          0.height,
+          FBString(
+            textController: controller.amountController,
+            label: 'amount'.tr,
+            //example: 'sample_model'.tr,
+            hint: 'amount'.tr,
+            isRequired: false,
+            errorMsg: '',
+          ),
+        ],
+      ),
+    );
   }
 }
