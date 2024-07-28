c7c8ef705e6ee71100f727cda9d605caa41de8a2
saiful <eamon831@gmail.com>
2024-07-28 19:28:12 +0600

    small refactor

--- lib/app/pages/inventory/purchase/create_purchase/controllers/create_purchase_controller.dart
+++ lib/app/pages/inventory/purchase/create_purchase/controllers/create_purchase_controller.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '/app/routes/app_pages.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 import '/app/core/abstract_controller/stock_selection_controller.dart';
 import '/app/entity/purchase_item.dart';
 import '/app/entity/stock.dart';
 import '/app/pages/inventory/purchase/create_purchase/modals/purchase_process_modal/purchase_process_view.dart';
+import '/app/routes/app_pages.dart';
 
 class CreatePurchaseController extends StockSelectionController {
   String? purchaseMode;
