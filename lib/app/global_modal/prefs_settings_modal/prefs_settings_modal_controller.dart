87945aa3c25e8ac5fb863baf2b3c13d242eca534
saiful <eamon831@gmail.com>
2024-07-29 15:04:16 +0600

    prefs modal updated

--- lib/app/global_modal/prefs_settings_modal/prefs_settings_modal_controller.dart
+++ lib/app/global_modal/prefs_settings_modal/prefs_settings_modal_controller.dart
@@ -1,5 +1,16 @@
+import 'package:get/get.dart';
 import 'package:sandra/app/core/base/base_controller.dart';
 
-class PrefsSettingsModalController extends BaseController{
+class PrefsSettingsModalController extends BaseController {
+  final isSalesOnline = false.obs;
+  final isZeroSalesAllowed = false.obs;
+  final printerType = ''.obs;
 
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    isSalesOnline.value = await prefs.getIsSalesOnline();
+    isZeroSalesAllowed.value = await prefs.getIsZeroSalesAllowed();
+    printerType.value = await prefs.getPrintPaperType();
+  }
 }
--- lib/app/global_modal/prefs_settings_modal/prefs_settings_modal_view.dart
+++ lib/app/global_modal/prefs_settings_modal/prefs_settings_modal_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sandra/app/core/base/base_view.dart';
 
 import 'prefs_settings_modal_controller.dart';
@@ -8,15 +9,82 @@ class PrefsSettingsModalView extends BaseView<PrefsSettingsModalController> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-
-          ],
-        ),
-      ),
+    return GetX<PrefsSettingsModalController>(
+      init: PrefsSettingsModalController(),
+      builder: (controller) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                         Text(
+                          appLocalization.isSalesOnline,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Switch(
+                          value: controller.isSalesOnline.value,
+                          onChanged: (value) async {
+                            controller.isSalesOnline.value = value;
+                            await controller.prefs.setIsSalesOnline(
+                              isSalesOnline: value,
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          appLocalization.isZeroSalesAllowed,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Switch(
+                          value: controller.isZeroSalesAllowed.value,
+                          onChanged: (value) async {
+                            controller.isZeroSalesAllowed.value = value;
+                            await controller.prefs.setIsZeroSalesAllowed(
+                              isZeroAllowed: value,
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const Text('Print Paper Type'),
+                if (controller.printerType.value.isEmpty)
+                  const CircularProgressIndicator()
+                else
+                  DropdownButton<String>(
+                    value: controller.printerType.value,
+                    items: <String>['80 mm', '58 mm']
+                        .map<DropdownMenuItem<String>>(
+                          (String value) => DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) async {
+                      controller.printerType.value = value!;
+                      await controller.prefs.setPrintPaperType(
+                        value,
+                      );
+                    },
+                  ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
