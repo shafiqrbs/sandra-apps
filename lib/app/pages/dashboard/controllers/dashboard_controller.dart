14772a5519612ed9177822602a852edbdd9e3b9c
EftiarHKhan <mdeftiar.official@gmail.com>
2024-10-08 22:21:41 +0600

    remove restaurant section from inventory tab

--- lib/app/pages/dashboard/controllers/dashboard_controller.dart
+++ lib/app/pages/dashboard/controllers/dashboard_controller.dart
@@ -104,12 +104,6 @@ List<Widget> inventoryButtonList = [
     localeMethod: () => appLocalization.purchaseReturn,
     bgColor: colorList.value['red'],
   ),
-  TbdRoundButton(
-    icon: TablerIcons.building_warehouse,
-    onTap: () => navigatePage(Routes.restaurantHome),
-    localeMethod: () => appLocalization.restaurant,
-    bgColor: colorList.value['green'],
-  ),
 ];
 
 List<Widget> accountingButtonList = [
