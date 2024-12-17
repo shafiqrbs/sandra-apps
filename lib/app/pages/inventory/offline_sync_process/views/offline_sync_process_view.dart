b89c7dab67c5812cbe7b3e0000ed23fc4e0f2ec6
MD SAIFUL HOSSAIN <mdsaiful.rbs@gmail.com>
2024-12-17 18:08:13 +0600

    WIP

--- lib/app/pages/inventory/offline_sync_process/views/offline_sync_process_view.dart
+++ lib/app/pages/inventory/offline_sync_process/views/offline_sync_process_view.dart
@@ -1,18 +1,6 @@
 import 'package:sandra/app/core/importer.dart';
-import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
-import 'package:nb_utils/nb_utils.dart';
-import 'package:sandra/app/core/core_model/page_state.dart';
-import 'package:sandra/app/core/values/app_strings.dart';
-import 'package:sandra/app/core/widget/common_icon_text.dart';
-import 'package:sandra/app/core/widget/delete_button.dart';
-import 'package:sandra/app/core/widget/no_record_found_view.dart';
-import 'package:sandra/app/core/widget/page_back_button.dart';
-import 'package:sandra/app/core/widget/quick_navigation_button.dart';
-import 'package:sandra/app/core/widget/retry_view.dart';
 import 'package:sandra/app/entity/sync_list.dart';
-import '/app/core/base/base_view.dart';
+
 import '/app/pages/inventory/offline_sync_process/controllers/offline_sync_process_controller.dart';
 
 //ignore: must_be_immutable
@@ -118,7 +106,10 @@ class OfflineSyncProcessView extends BaseView<OfflineSyncProcessController> {
           )
         : '';
     return InkWell(
-      onTap: () {},
+      onTap: () => controller.approve(
+        element: element,
+        index: index,
+      ),
       child: Container(
         margin: EdgeInsets.only(
           left: 8,
