5ca3ba5bce696722445732d2c76e337e44ec7d47
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2024-12-29 20:41:11 +0600

    WIP

--- lib/app/global_widget/video_player_widget.dart
+++ lib/app/global_widget/video_player_widget.dart
@@ -1,6 +1,6 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'package:chewie/chewie.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
